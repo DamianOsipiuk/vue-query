f86d2680e34d79b884d6a732eb9f5c14b3e692be
Damian Osipiuk <osipiukd+git@gmail.com>
2021-04-14 17:29:18 +0200

    refactor: split sort functions, use methods from query instead of states

--- src/devtools/utils.ts
+++ src/devtools/utils.ts
@@ -9,17 +9,10 @@ export enum Position {
   BL = "bottom-left",
 }
 
-export function isStale(query: Query): boolean {
-  return typeof query.isStale === "function"
-    ? query.isStale()
-    : // @ts-expect-error Invalid types
-      query.state.isStale;
-}
-
 export function getQueryStatusColor(query: Query, theme: Theme): string {
-  return query.state.isFetching
+  return query.isFetching()
     ? theme.active
-    : isStale(query)
+    : query.isStale()
     ? theme.warning
     : // @ts-expect-error Invalid types
     !query.observers.length
@@ -28,12 +21,12 @@ export function getQueryStatusColor(query: Query, theme: Theme): string {
 }
 
 export function getQueryStatusLabel(query: Query): string {
-  return query.state.isFetching
+  return query.isFetching()
     ? "fetching"
     : // @ts-expect-error Invalid types
     !query.observers.length
     ? "inactive"
-    : isStale(query)
+    : query.isStale()
     ? "stale"
     : "fresh";
 }
@@ -44,20 +37,26 @@ export const getStatusRank = (query: Query): number =>
     : // @ts-expect-error Invalid types
     !query.observers.length
     ? 3
-    : isStale(query)
+    : query.isStale()
     ? 2
     : 1;
 
 export type SortFn = (a: Query, b: Query) => number;
 
+const queryHashSort: SortFn = (a, b) => (a.queryHash > b.queryHash ? 1 : -1);
+
+const dateSort: SortFn = (a, b) =>
+  a.state.dataUpdatedAt < b.state.dataUpdatedAt ? 1 : -1;
+
+const statusAndDateSort: SortFn = (a, b) =>
+  getStatusRank(a) === getStatusRank(b)
+    ? dateSort(a, b)
+    : getStatusRank(a) > getStatusRank(b)
+    ? 1
+    : -1;
+
 export const sortFns: Record<string, SortFn> = {
-  "Status > Last Updated": (a, b) =>
-    getStatusRank(a) === getStatusRank(b)
-      ? sortFns["Last Updated"](a, b)
-      : getStatusRank(a) > getStatusRank(b)
-      ? 1
-      : -1,
-  "Query Hash": (a, b) => (a.queryHash > b.queryHash ? 1 : -1),
-  // @ts-expect-error Invalid types
-  "Last Updated": (a, b) => (a.state.updatedAt < b.state.updatedAt ? 1 : -1),
+  "Status > Last Updated": statusAndDateSort,
+  "Query Hash": queryHashSort,
+  "Last Updated": dateSort,
 };
