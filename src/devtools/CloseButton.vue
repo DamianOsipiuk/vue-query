d63aa494a5ee787e235b835b7f34b750d2876d5b
Damian Osipiuk <osipiukd+git@gmail.com>
2021-04-14 17:13:41 +0200

    refactor: precompute position prop in CloseButton

--- src/devtools/CloseButton.vue
+++ src/devtools/CloseButton.vue
@@ -23,26 +23,20 @@ export default defineComponent({
       props.buttonProps.click?.();
     };
 
+    const position = computed(() => {
+      if (props.position === Position.TR || props.position === Position.BR) {
+        return { right: 0 };
+      } else {
+        return { left: 0 };
+      }
+    });
+
     const buttonStyles = computed(() => ({
       position: "fixed",
       zIndex: "99999",
       margin: ".5rem",
       bottom: 0,
-      ...(props.position === Position.TR
-        ? {
-            right: "0",
-          }
-        : props.position === Position.TL
-        ? {
-            left: "0",
-          }
-        : props.position === Position.BR
-        ? {
-            right: "0",
-          }
-        : {
-            left: "0",
-          }),
+      ...position.value,
       ...props.buttonProps.style,
     }));
 
