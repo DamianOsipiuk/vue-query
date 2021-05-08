dab18a3a2a727086ca71513dfb3305de8e26a37a
Damian Osipiuk <osipiukd+git@gmail.com>
2021-05-09 00:57:25 +0200

    test: hide vue2 irrelevant warnings

--- src/__mocks__/vue-demi.ts
+++ src/__mocks__/vue-demi.ts
@@ -1,3 +1,8 @@
+// Hide annoying console warnings for Vue2
+import Vue from "vue2";
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
 const vue = jest.requireActual("vue-demi");
 
 module.exports = {
