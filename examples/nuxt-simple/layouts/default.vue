517c82000c4c0291dcb8154a8e86f831729e7cb0
Damian Osipiuk <osipiukd+git@gmail.com>
2021-06-08 18:35:05 +0200

    docs: nuxt example

---
+++ examples/nuxt-simple/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt />
+    <VueQueryDevTools />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import { useQueryProvider } from "vue-query";
+import { VueQueryDevTools } from "vue-query/devtools";
+
+export default defineComponent({
+  components: { VueQueryDevTools },
+  setup() {
+    useQueryProvider();
+  },
+});
+</script>
---
+++ examples/nuxt-simple/pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Index - Hi from {{ renderer }}</h1>
+    <nuxt-link to="/about">About page</nuxt-link>
+    <button @click="refetch">Refetch</button>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+import {
+  defineComponent,
+  ssrRef,
+  useContext,
+  onServerPrefetch,
+  onBeforeMount,
+} from "@nuxtjs/composition-api";
+import {
+  hydrate,
+  dehydrate,
+  QueryClient,
+  useQuery,
+  useQueryClient,
+} from "vue-query";
+
+const fetcher = async () =>
+  await fetch("https://jsonplaceholder.typicode.com/todos").then((response) =>
+    response.json()
+  );
+
+export default defineComponent({
+  setup(props, attrs) {
+    const renderer = ssrRef("client");
+
+    const { refetch, data } = useQuery("todos", fetcher, {
+      enabled: false,
+    });
+
+    onBeforeMount(() => {
+      const { nuxtState } = useContext();
+      const queryClient = useQueryClient();
+      hydrate(queryClient, nuxtState.vueQueryState);
+    });
+
+    onServerPrefetch(async () => {
+      renderer.value = "server";
+
+      const { ssrContext } = useContext();
+      const queryClient = new QueryClient();
+      await queryClient.prefetchQuery("todos", fetcher);
+
+      ssrContext.nuxt.vueQueryState = dehydrate(queryClient);
+    });
+
+    return {
+      refetch,
+      renderer,
+      data,
+    };
+  },
+});
+</script>
