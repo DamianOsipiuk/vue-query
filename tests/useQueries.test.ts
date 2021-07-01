880577e7a9065f19a8a739714fb8803ddb3014c5
Damian Osipiuk <osipiukd+git@gmail.com>
2021-07-02 00:20:11 +0200

    fix(hooks): useQueries reactivity

---
+++ tests/useQueries.test.ts
@@ -0,0 +1,154 @@
+import { reactive, set } from "vue-demi";
+import { setLogger } from "react-query/core";
+import { useQueries } from "../src/useQueries";
+import {
+  flushPromises,
+  rejectFetcher,
+  simpleFetcher,
+  noop,
+} from "./test-utils";
+
+jest.mock("../src/useQueryClient");
+
+describe("useQueries", () => {
+  beforeAll(() => {
+    setLogger({ log: noop, warn: noop, error: noop });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should return result for each query", () => {
+    const queries = [
+      {
+        queryKey: "key1",
+        queryFn: simpleFetcher,
+      },
+      {
+        queryKey: "key2",
+        queryFn: simpleFetcher,
+      },
+    ];
+    const queriesState = useQueries(queries);
+
+    expect(queriesState).toMatchObject([
+      {
+        status: "loading",
+        isLoading: true,
+        isFetching: true,
+        isStale: true,
+      },
+      {
+        status: "loading",
+        isLoading: true,
+        isFetching: true,
+        isStale: true,
+      },
+    ]);
+  });
+
+  test("should resolve to success and update reactive state", async () => {
+    const queries = [
+      {
+        queryKey: "key1",
+        queryFn: simpleFetcher,
+      },
+      {
+        queryKey: "key2",
+        queryFn: simpleFetcher,
+      },
+    ];
+    const queriesState = useQueries(queries);
+
+    await flushPromises();
+
+    expect(queriesState).toMatchObject([
+      {
+        status: "success",
+        isLoading: false,
+        isFetching: false,
+        isStale: true,
+      },
+      {
+        status: "success",
+        isLoading: false,
+        isFetching: false,
+        isStale: true,
+      },
+    ]);
+  });
+
+  test("should reject one of the queries and update reactive state", async () => {
+    const queries = [
+      {
+        queryKey: "key1",
+        queryFn: rejectFetcher,
+        retry: false,
+      },
+      {
+        queryKey: "key2",
+        queryFn: simpleFetcher,
+      },
+    ];
+    const queriesState = useQueries(queries);
+
+    await flushPromises();
+
+    expect(queriesState).toMatchObject([
+      {
+        status: "error",
+        isLoading: false,
+        isFetching: false,
+        isStale: true,
+      },
+      {
+        status: "success",
+        isLoading: false,
+        isFetching: false,
+        isStale: true,
+      },
+    ]);
+  });
+
+  test("should return state for new queries", async () => {
+    const initialQueries = reactive([]);
+    const queries = [
+      {
+        queryKey: "key1",
+        queryFn: simpleFetcher,
+      },
+      {
+        queryKey: "key2",
+        queryFn: simpleFetcher,
+      },
+    ];
+    const queriesState = useQueries(initialQueries);
+
+    expect(queriesState.length).toEqual(0);
+
+    queries.forEach((query, index) => {
+      set(initialQueries, index, query);
+    });
+
+    await flushPromises();
+    await flushPromises();
+
+    expect(queriesState.length).toEqual(2);
+
+    expect(queriesState).toMatchObject([
+      {
+        status: "success",
+        isLoading: false,
+        isFetching: false,
+        isStale: true,
+      },
+      {
+        status: "success",
+        isLoading: false,
+        isFetching: false,
+        isStale: true,
+      },
+    ]);
+  });
+});
