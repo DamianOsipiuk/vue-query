731591088dc59d8fce6c5d3ec27272c51b0cfd2c
Damian Osipiuk <osipiukd+git@gmail.com>
2022-01-22 17:15:43 +0100

    build: remove plugin, add external deps

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,18 @@
 import resolve from "@rollup/plugin-node-resolve";
-import autoExternal from "rollup-plugin-auto-external";
 import postcss from "rollup-plugin-postcss";
 import typescript from "rollup-plugin-typescript2";
 import vue from "rollup-plugin-vue";
 
 const common = {
-  plugins: [resolve(), autoExternal(), typescript(), vue(), postcss()],
-  external: ["vue-query", "react-query/core", "vue", "vue-demi"],
+  plugins: [resolve(), typescript(), vue(), postcss()],
+  external: [
+    "vue-query",
+    "react-query/core",
+    "vue",
+    "vue-demi",
+    "match-sorter",
+    "@nuxtjs/composition-api",
+  ],
   watch: {
     include: "src/**",
     exclude: ["node_modules/**", "tests"],
