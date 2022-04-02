6adde50ef30ea602d0efb5b7df163c61856f33c9
Damian Osipiuk <osipiukd+git@gmail.com>
2022-04-02 23:13:55 +0200

    fix: basic codesandbox

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -5,5 +5,5 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   // For dev purpose, when using `npm link`. This breaks codesandbox somehow.
-  optimizeDeps: { exclude: ["vue-query", "vue-demi"] },
+  // optimizeDeps: { exclude: ["vue-query", "vue-demi"] },
 });
