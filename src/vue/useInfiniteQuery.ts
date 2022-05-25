896344b9bd8245a7fa6efd1e5a48a8bd94477e9f
Damian Osipiuk <osipiukd+git@gmail.com>
2022-05-26 00:42:10 +0200

    feat: unwrap queryFn key type, update query/core

--- src/vue/useInfiniteQuery.ts
+++ src/vue/useInfiniteQuery.ts
@@ -1,5 +1,5 @@
 import { InfiniteQueryObserver } from "react-query/core";
-
+import type { UnwrapRef } from "vue-demi";
 import type {
   QueryObserver,
   QueryFunction,
@@ -64,7 +64,7 @@ export function useInfiniteQuery<
   TQueryKey extends QueryKey = QueryKey
 >(
   queryKey: TQueryKey,
-  queryFn: QueryFunction<TQueryFnData, TQueryKey>,
+  queryFn: QueryFunction<TQueryFnData, UnwrapRef<TQueryKey>>,
   options?: Omit<
     UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
     "queryKey" | "queryFn"
@@ -81,7 +81,7 @@ export function useInfiniteQuery<
     | TQueryKey
     | UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
   arg2?:
-    | QueryFunction<TQueryFnData, TQueryKey>
+    | QueryFunction<TQueryFnData, UnwrapRef<TQueryKey>>
     | UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
   arg3?: UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryKey>
 ): UseInfiniteQueryReturnType<TData, TError> {
