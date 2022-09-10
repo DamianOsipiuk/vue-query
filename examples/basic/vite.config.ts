5ee149b605f579c6f6e1c9da71bbc0f33fe42ba8
Damian Osipiuk <osipiukd+git@gmail.com>
2022-09-10 13:38:11 +0200

    feat: update to esm compliant query-core

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -4,9 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // For dev purpose, when using `npm link`. This breaks codesandbox somehow.
   optimizeDeps: {
-    // include: ["remove-accents"],
-    // exclude: ["vue-query", "vue-demi"],
+    include: ["remove-accents"],
+    exclude: ["vue-query", "vue-demi"],
   },
 });
